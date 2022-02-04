5613f56f27760ca4ee11d7b216ff405b3774b6b6
vivek-437 <vievkgupta2742003@gmail.com>
2022-02-04 19:47:56 +0530

    only validation and post part remain

--- src/components/js/Form.jsx
+++ src/components/js/Form.jsx
@@ -1,48 +1,91 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Singledata from "./Singledata";
+import "../css/form.css";
 const Form = () => {
-  // passing data from useeffect to setList usestate
-  const [list, setList] = useState({});
   //   title
-  const [title, setTitle] = useState("");
+  const [Title, setTitle] = useState("");
   // body
-  const [body, setBody] = useState("");
-  // user
-  const [user, setUser] = useState({});
-
+  const [Body, setBody] = useState("");
+  // username
+  const [username, setUsername] = useState([]);
+  // newdata
+  const [allentry, setAllentry] = useState([]);
+  const [Id, setId] = useState([]);
   // onclick on submit
-  const submit = () => {};
-  //
-  const showList = () => {};
+  const submit = (e) => {
+    // removing auto refresh on submit
+    e.preventDefault();
+    // console.log(e);
+    const Newentry = { id: Id, title: Title, body: Body };
+    setAllentry([...allentry, Newentry]);
+    post();
+
+  };
+
+  const post=()=>{
+    allentry.map((val)=>(
+    axios.post("https://jsonplaceholder.typicode.com/posts",{
+      userId:val.id,
+      title:val.title,
+      body:val.body
+    }).then((result)=>{
+      console.log(result.data)
+    })
+    ))
+
+
+
+  }
   // getting info from below link
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((respones) => {
-      setList(respones.data.map((data) => ({ id: data.id, name: data.name })));
-    });
+    const fetchData = async () => {
+      const request = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // passing url data to usestate
+      setUsername(
+        request.data.map((users) => ({ name: users.name, id: users.id }))
+      );
+      return request;
+    };
+    fetchData();
   }, []);
 
-  console.log(list);
   return (
     <>
-      <div>
-        <select name="list" id="" onChange={showList} options={list}>
-            
-        </select>
-        <form>
-          <input
-            type="text"
-            value={title}
-            onChange={(userdata) => setTitle(userdata)}
-          />
-          <input
-            type="text"
-            value={body}
-            onChange={(userdata) => setBody(userdata)}
-          />
-          <button type="submit" onClick={submit}>
-            Submit
-          </button>
+      <div className="container">
+        <form onSubmit={submit}>
+          <div className="select">
+            <label>Please select a username:-</label>
+            <select name="" onChange={(e) => setId(e.target.value)}>
+              {username?.map((val) => (
+                <option value={val.id}>{val.name}</option>
+              ))}
+            </select>
+          </div>
+          <div className="title">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              id="title"
+              autoComplete="off"
+              name="title"
+            />
+          </div>
+          <div className="body">
+            <label htmlFor="body">Body</label>
+            <input
+              type="text"
+              onChange={(e) => setBody(e.target.value)}
+              id="body"
+              autoComplete="off"
+              name="body"
+            />
+          </div>
+          <div className="submit">
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     </>
